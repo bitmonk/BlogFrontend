fd101f4e899daf15e08911f26219fcb530dfbe6a
Parbat Tamang <parbattamang1234@gmail.com>
2024-09-29 15:40:24 +0545

    minor changes

--- src/pages/EditBlog.jsx
+++ src/pages/EditBlog.jsx
@@ -1,5 +1,5 @@
 import { useNavigate, useParams } from "react-router-dom"
-import Navbar from "../components/Navbar.jsx"
+import Navbar from "../components/Navbar"
 import { useEffect, useState } from "react"
 import axios from "axios"
 
